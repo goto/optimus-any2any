1ef8a7fa18cfe396a3dac7d665d05f9115d80138
Ahmad N. F. <fahmadnaufal@gmail.com>
2025-05-02 00:03:37 +0700

    integrate with s3

--- ext/s3/sink.go
+++ ext/s3/sink.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"net/url"
-	"os"
 	"path/filepath"
 	"strings"
 	"text/template"
@@ -25,16 +24,15 @@ import (
 type S3Sink struct {
 	common.Sink
 
-	client                    *S3Client
-	destinationURITemplate    *template.Template
-	writeHandlers             map[string]xio.WriteFlusher // tmp write handler
-	s3Handlers                map[string]io.WriteCloser
-	fileRecordCounters        map[string]int
-	batchSize                 int
-	enableOverwrite           bool
-	skipHeader                bool
-	maxTempFileRecordNumber   int
-	partialFileRecordCounters map[string]int // map of destinationURI to the number of records
+	client                  *S3Client
+	destinationURITemplate  *template.Template
+	writeHandlers           map[string]xio.WriteFlusher // tmp write handler
+	s3Handlers              map[string]io.WriteCloser
+	fileRecordCounters      map[string]int
+	batchSize               int
+	enableOverwrite         bool
+	skipHeader              bool
+	maxTempFileRecordNumber int
 }
 
 var _ flow.Sink = (*S3Sink)(nil)
@@ -75,17 +73,16 @@ func NewSink(commonSink common.Sink,
 	}
 
 	s3 := &S3Sink{
-		Sink:                      commonSink,
-		client:                    client,
-		destinationURITemplate:    tmpl,
-		writeHandlers:             make(map[string]xio.WriteFlusher),
-		s3Handlers:                make(map[string]io.WriteCloser),
-		fileRecordCounters:        make(map[string]int),
-		batchSize:                 batchSize,
-		enableOverwrite:           enableOverwrite,
-		skipHeader:                skipHeader,
-		maxTempFileRecordNumber:   maxTempFileRecordNumber,
-		partialFileRecordCounters: make(map[string]int),
+		Sink:                    commonSink,
+		client:                  client,
+		destinationURITemplate:  tmpl,
+		writeHandlers:           make(map[string]xio.WriteFlusher),
+		s3Handlers:              make(map[string]io.WriteCloser),
+		fileRecordCounters:      make(map[string]int),
+		batchSize:               batchSize,
+		enableOverwrite:         enableOverwrite,
+		skipHeader:              skipHeader,
+		maxTempFileRecordNumber: maxTempFileRecordNumber,
 	}
 
 	// add clean func
@@ -94,18 +91,6 @@ func NewSink(commonSink common.Sink,
 		var e error
 		for _, handler := range s3.s3Handlers {
 			err := handler.Close()
-			e = errs.Join(e, err)
-		}
-		return e
-	})
-	commonSink.AddCleanFunc(func() error {
-		s3.Logger().Info("remove tmp files")
-		var e error
-		for tmpPath := range s3.writeHandlers {
-			err := os.Remove(tmpPath)
-			if errors.Is(err, os.ErrNotExist) {
-				continue
-			}
 			e = errs.Join(e, err)
 		}
 		return e
@@ -139,21 +124,8 @@ func (s3 *S3Sink) process() error {
 			destinationURI = getDestinationURIByBatch(destinationURI, recordCounter, s3.batchSize)
 		}
 
-		// stream to tmp file
-		tmpPath, err := getTmpPath(destinationURI)
-		if err != nil {
-			s3.Logger().Error(fmt.Sprintf("failed to get tmp URI: %s", destinationURI))
-			return errors.WithStack(err)
-		}
-		wh, ok := s3.writeHandlers[tmpPath]
+		wh, ok := s3.writeHandlers[destinationURI]
 		if !ok {
-			// create new tmp write handler
-			wh, err = xio.NewWriteHandler(s3.Logger(), tmpPath)
-			if err != nil {
-				s3.Logger().Error(fmt.Sprintf("failed to create tmp write handler: %s", err.Error()))
-				return errors.WithStack(err)
-			}
-
 			// create new s3 write handler
 			targetDestinationURI, err := url.Parse(destinationURI)
 			if err != nil {
@@ -176,9 +148,14 @@ func (s3 *S3Sink) process() error {
 			}
 			var sh io.WriteCloser = s3.client.GetUploadWriter(s3.Context(), targetDestinationURI.Host, strings.TrimLeft(targetDestinationURI.Path, "/"))
 
-			// dual handlers for both tmp file and s3
-			s3.writeHandlers[tmpPath] = wh
+			// store in both write handlers & s3 handlers
+			// so that s3 write handler can be closed
 			s3.s3Handlers[destinationURI] = sh
+			s3.writeHandlers[destinationURI] = xio.NewChunkWriter(
+				s3.Logger(), sh,
+				xio.WithExtension(filepath.Ext(destinationURI)),
+				xio.WithCSVSkipHeader(s3.skipHeader),
+			)
 		}
 
 		// record without metadata
@@ -196,37 +173,9 @@ func (s3 *S3Sink) process() error {
 		}
 
 		recordCounter++
-		s3.fileRecordCounters[tmpPath]++
-		s3.partialFileRecordCounters[destinationURI]++
+		s3.fileRecordCounters[destinationURI]++
 		if recordCounter%logCheckPoint == 0 {
-			s3.Logger().Info(fmt.Sprintf("written %d records to tmp file: %s", s3.fileRecordCounters[tmpPath], tmpPath))
-		}
-
-		// EXPERIMENTAL: flush and upload to S3 if maximum number of records in tmp file is reached.
-		// a method of partial upload is implemented to avoid having large number of tmp files which leads to high disk usage.
-		// as long as the streamed records from source are guaranteed to be uniform (same header & value ordering), this should work.
-		if s3.maxTempFileRecordNumber > 0 && s3.partialFileRecordCounters[destinationURI] >= s3.maxTempFileRecordNumber {
-			s3.Logger().Info(fmt.Sprintf("maximum number of temp file records %d reached. flushing %s to OSS", s3.maxTempFileRecordNumber, tmpPath))
-			if err := wh.Flush(); err != nil {
-				s3.Logger().Error(fmt.Sprintf("failed to flush tmp file: %s", tmpPath))
-				return errors.WithStack(err)
-			}
-			s3.Logger().Info(fmt.Sprintf("flushed tmp file: %s", tmpPath))
-
-			if err := s3.flush(destinationURI, s3.s3Handlers[destinationURI]); err != nil {
-				s3.Logger().Error(fmt.Sprintf("failed to upload to OSS: %s", destinationURI))
-				return errors.WithStack(err)
-			}
-
-			// reset counters and handlers for the current batch
-			// except for s3 handler which is reused to upload the next part of the file
-			s3.partialFileRecordCounters[destinationURI] = 0
-			delete(s3.writeHandlers, tmpPath)
-
-			if err := os.Remove(tmpPath); err != nil && !errors.Is(err, os.ErrNotExist) {
-				s3.Logger().Error(fmt.Sprintf("failed to remove tmp file: %s", tmpPath))
-				return errors.WithStack(err)
-			}
+			s3.Logger().Info(fmt.Sprintf("written %d records to file writer: %s", s3.fileRecordCounters[destinationURI], destinationURI))
 		}
 	}
 	if recordCounter == 0 {
@@ -245,9 +194,14 @@ func (s3 *S3Sink) process() error {
 
 	// flush remaining records concurrently
 	funcs := []func() error{}
-	for uri := range s3.s3Handlers {
+	for destinationURI, wh := range s3.writeHandlers {
 		funcs = append(funcs, func() error {
-			return s3.flush(uri, s3.s3Handlers[uri])
+			if err := wh.Flush(); err != nil {
+				s3.Logger().Error(fmt.Sprintf("failed to flush to %s", destinationURI))
+				return errors.WithStack(err)
+			}
+			s3.Logger().Info(fmt.Sprintf("flushed file: %s", destinationURI))
+			return nil
 		})
 	}
 	if err := s3.ConcurrentTasks(s3.Context(), 4, funcs); err != nil {
@@ -255,61 +209,6 @@ func (s3 *S3Sink) process() error {
 	}
 
 	s3.Logger().Info(fmt.Sprintf("successfully written %d records", recordCounter))
-	return nil
-}
-
-func (s3 *S3Sink) flush(destinationURI string, wh io.WriteCloser) error {
-	tmpPath, err := getTmpPath(destinationURI)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	// open tmp file for reading
-	var tmpReader io.ReadSeekCloser
-	tmpReader, err = os.OpenFile(tmpPath, os.O_RDONLY, 0644)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	// header is skipped if SKIP_HEADER is explicitly set to true OR if file has been partially uploaded previously
-	// skipHeader := s3.skipHeader || (s3.maxTempFileRecordNumber > 0 && s3.fileRecordCounters[tmpPath] > s3.maxTempFileRecordNumber)
-
-	// convert to appropriate format if necessary
-	cleanUpFn := func() error { return nil }
-	switch filepath.Ext(destinationURI) {
-	case ".json":
-		// do nothing
-	// case ".csv":
-	// 	tmpReader, cleanUpFn, err = helper.FromJSONToCSV(s3.Logger(), tmpReader, skipHeader)
-	// case ".tsv":
-	// 	tmpReader, cleanUpFn, err = helper.FromJSONToCSV(s3.Logger(), tmpReader, skipHeader, rune('\t'))
-	// case ".xlsx":
-	// 	tmpReader, cleanUpFn, err = helper.FromJSONToXLSX(s3.Logger(), tmpReader, skipHeader)
-	default:
-		s3.Logger().Warn(fmt.Sprintf("unsupported file format: %s, use default (json)", filepath.Ext(destinationURI)))
-		// do nothing
-	}
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	defer func() {
-		cleanUpFn()
-		tmpReader.Close()
-	}()
-
-	// remove tmp file if destination is csv or tsv
-	if filepath.Ext(destinationURI) == ".csv" || filepath.Ext(destinationURI) == ".tsv" {
-		if err := os.Remove(tmpPath); err != nil {
-			return errors.WithStack(err)
-		}
-	}
-	defer tmpReader.Close()
-
-	s3.Logger().Info(fmt.Sprintf("upload tmp file %s to s3 %s", tmpPath, destinationURI))
-	n, err := io.Copy(wh, tmpReader)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
-	s3.Logger().Debug(fmt.Sprintf("uploaded %d bytes to s3 %s", n, destinationURI))
 	return nil
 }
 
