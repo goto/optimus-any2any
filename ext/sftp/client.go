94b0783a96ae5274c3002af2022c1fc72330c5d5
Abram Perdanaputra <abram.perdanaputra@gojek.com>
2025-06-18 11:12:59 +0700

    feat: increase timeout

--- ext/sftp/client.go
+++ ext/sftp/client.go
@@ -17,7 +17,10 @@ func newClient(address, username, password, privateKey, hostFingerprint string) 
 		User:            username,
 		Auth:            []ssh.AuthMethod{},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		Timeout:         5 * time.Second,
+		Timeout:         5 * time.Minute,
+		HostKeyAlgorithms: []string{
+			ssh.KeyAlgoRSA,
+		},
 	}
 	if password != "" {
 		config.Auth = append(config.Auth, ssh.Password(password))
