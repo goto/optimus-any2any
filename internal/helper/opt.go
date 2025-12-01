6ddaa96d1af0f392112ca7f000cfbd950540b1a2
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2025-12-01 12:19:03 +0700

    remove: unecessary code

--- internal/helper/opt.go
+++
@@ -1,9 +0,0 @@
-package helper
-
-type RecordReaderOption func(*RecordReader)
-
-func WithLogCheckpoint(count int) RecordReaderOption {
-	return func(r *RecordReader) {
-		r.logCheckpoint = count
-	}
-}
